fb55443fd3422263337b174b92ca00f250eb5f08
Sandesh-Veer <sandeshveer44@.com>
2026-08-04 22:29:30 +0530

    Describe what you changed here

--- BookManagement/server/database.js
+++ BookManagement/server/database.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 
 async function databaseConnection() {
   try {
-    await mongoose.connect(
-      "mongodb://sandeshveer44_db_user:sandesh123@ac-fbhztjl-shard-00-00.gmqiscl.mongodb.net:27017,ac-fbhztjl-shard-00-01.gmqiscl.mongodb.net:27017,ac-fbhztjl-shard-00-02.gmqiscl.mongodb.net:27017/?ssl=true&replicaSet=atlas-1026bx-shard-0&authSource=admin&appName=Cluster0"
-    );
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      throw new Error('MONGO_URI environment variable is not set');
+    }
+
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log("✅ Database connected");
   } catch (err) {
